eed6bb81ade9c5747a6f7deafb74f927b60d546d
utkarshsakarwar <109698025+utkarshsakarwar@users.noreply.github.com>
2022-09-13 20:14:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -354,16 +354,7 @@ sort_btn.addEventListener("click", function () {
       bubbleSort(unsorted_array);
       break;
     case "merge":
-      if (
-        confirm(
-          "Merge Sort is not visualized properly. Do you want to continue?"
-        )
-      ) {
-        mergeSort(unsorted_array);
-      } else {
-        break;
-      }
-      //console.log(mergeSort(unsorted_array));
+      mergeSort(unsorted_array);
       break;
     case "heap":
       HeapSort(unsorted_array);
